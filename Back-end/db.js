9de375d861d1b99fb1078dd15c9b90380c59552f
Mais Ghaith <117035615+MaisGhaith@users.noreply.github.com>
2023-06-02 23:21:33 +0300

    add encryption

--- Back-end/db.js
+++ Back-end/db.js
@@ -3,10 +3,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'Sphack!123',
+    password: 'Maisghaith21@@',
     host: 'localhost',
     port: 5432,
-    database: 'mahmoud',
+    database: 'GOAT-Payment',
 });
 
 module.exports = pool;

d07665741fa09d1de6d269e412f7b5a09d9e4f65
Mais Ghaith <117035615+MaisGhaith@users.noreply.github.com>
2023-06-02 14:39:06 +0300

    edit

--- Back-end/db.js
+++ Back-end/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'Maisghaith21@@',
+    password: '@@',
     host: 'localhost',
     port: 5432,
     database: 'GOAT-Payment',

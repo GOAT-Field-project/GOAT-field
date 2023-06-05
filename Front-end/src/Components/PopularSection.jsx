bb67b6b41ccf9c2c10c36ff4a44405e673b3cda7
MajdFoudeh <majdfoude@gmail.com>
2023-06-05 23:13:16 +0300

    amer edits

--- Front-end/src/Components/PopularSection.jsx
+++ Front-end/src/Components/PopularSection.jsx
@@ -60,14 +60,14 @@ const PopularSection = () => {
       <p>These popular destinations have a lot to offer</p>
       <CardContainer className="gap-y-10 gap-x-1">
         {featuredCityData.slice(0, 6).map((city) => (
-          <div key={city.id} style={{ width: "380px" }} >
-            <a href="#" style={{ height: "450px" }} className="block rounded-lg p-4 shadow-sm shadow-gray-300">
+          <div key={city.id} className="2xl:w-1/4 xl:w-1/3 lg:w-1/3 w-2/3 min-w-fit">
+            <a href="#" className="block rounded-lg p-4 shadow-sm shadow-gray-300">
 
               <img
+                className=" rounded-lg"
                 style={{
-                  width: "100%",
                   objectFit: "contain",
-                  height: "220px" // Set the desired height for the image
+                  height: "200px" // Set the desired height for the image
                 }}
                 src={`data:image/jpeg;base64,${city.images[0]}`}
                 alt={`Image 1`}
@@ -75,18 +75,34 @@ const PopularSection = () => {
               <div className="mt-2">
                 <dl>
                   <div>
-                    <dt className="sr-only">Address</dt>
-                    <dd className="font-medium">{city.name}</dd>
+                    <div className="flex items-start justify-start gap-2">
+                      <dd className="font-medium text-black">field name:</dd><span className="">{city.name}</span>
+                    </div>
+
                   </div>
                   <div>
                     <dt className="sr-only">Price</dt>
-                    <dd className="text-sm text-gray-500">Number of reservations: {city.name}</dd>
                   </div>
                   <br />
                   <div>
-                    <dt className="sr-only">Price</dt>
+                    <div className="flex items-start justify-start gap-2">
+                      <dd className="text-sm text-black   font-medium">$ price:</dd><span className="">{city.price}</span>
+                    </div>
+                  </div>
+                  <br />
+                  <div>
+                    <div className="flex items-start justify-start gap-2">
+                      <dd className="text-sm text-black   font-medium">Field size:</dd><span className="">{city.size}</span>
+                    </div>
+                  </div>
+                  <br />
 
-                    <dd style={{ width: "20vw" }} className="text-sm text-gray-500">DEtails:{city.details}</dd>
+                  <div>
+                    <div className="flex flex-col items-start justify-start gap-2 w-20 break-normal	pr-20">
+                      <dd className="text-sm  mt-1 text-black w-20  font-medium">details:</dd><span className="inline-block w-60 break-words">{city.details}</span>
+
+                    </div>
+                    <br />
 
                   </div>
                 </dl>
@@ -107,27 +123,12 @@ const PopularSection = () => {
                       />
                     </svg>
                     <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">City</p>
-                      <p className="font-medium">{city.city}</p>
+                      <p className="text-black">{city.location} </p>
+                      <p className="font-medium text-black"></p>
                     </div>
                   </div>
                   <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="h-4 w-4 text-[#54B435]"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M13 10V3L4 14h7v7l9-11h-7z"
-                      />
-                    </svg>
                     <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Hotels</p>
                       <p className="font-medium">{city.reservations}</p>
                     </div>
                   </div>
@@ -137,6 +138,7 @@ const PopularSection = () => {
           </div>
         ))}
       </CardContainer>
+
     </Container>
   );
 };
--- Front-end/src/Pages/Signup.jsx
+++ Front-end/src/Pages/Signup.jsx
@@ -131,6 +131,24 @@ export default function Signup() {
                 Login
               </Link>
             </p>
+
+            {errorResponse && (
+              <div className="relative m-2 my-8 max-w-sm rounded-lg border border-gray-100 bg-white px-12 py-6 shadow-md">
+                <button className="absolute top-0 right-0 p-4 text-gray-400">
+
+                </button>
+                <p className="relative mb-1 text-sm font-medium">
+                  <span className="absolute -left-7 flex h-5 w-5 items-center justify-center rounded-xl bg-red-400 text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-3 w-3">
+                      <path d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </span>
+                  <p className="text-sm text-red-600">{errorResponse}.</p>
+                </p>
+
+              </div>
+            )}
+
             <form
               className="flex flex-col pt-3 md:pt-8"
               onSubmit={handleSubmit}
@@ -199,9 +217,7 @@ export default function Signup() {
                     {errors.username}
                   </p>
                 )}
-                {errorResponse && (
-                  <p className="text-red-600">{errorResponse}</p>
-                )}
+
               </div>
               <div className="flex flex-col pt-4">
                 <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
@@ -231,9 +247,7 @@ export default function Signup() {
                     {errors.email}
                   </p>
                 )}
-                {errorResponse && (
-                  <p className="text-red-600">{errorResponse}</p>
-                )}
+
               </div>
               <div className="flex flex-col pt-4 mb-10">
                 <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
@@ -263,9 +277,7 @@ export default function Signup() {
                     {errors.password}
                   </p>
                 )}
-                {errorResponse && (
-                  <p className="text-red-600">{errorResponse}</p>
-                )}
+
               </div>
               <button
                 type="submit"
